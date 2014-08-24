582d2b5e2ab29db2e4a4e1d5950fd02c6bbe5cb6
Victor Bjelkholm <victorbjelkholm@gmail.com>
2014-08-24 18:46:25 +0200

    Rename WorldNews to Headlinie

--- Dockerfile
+++ Dockerfile
@@ -12,21 +12,21 @@ RUN mv composer.phar /usr/local/bin/composer
 
 # Application stuff
 
-RUN mkdir -p /worldnews/www
-ADD www/composer.json /worldnews/www/composer.json
-RUN (cd /worldnews/www; composer install -o)
-ADD . /worldnews/
-RUN (cd /worldnews/www; composer install -o)
+RUN mkdir -p /headlinie/api
+ADD www/composer.json /headlinie/api/composer.json
+RUN (cd /headlinie/api; composer install -o)
+ADD . /headlinie/
+RUN (cd /headlinie/api; composer install -o)
 
 # Configuration stuff
 
 RUN rm /etc/nginx/sites-enabled/default
-ADD configs/site_worldnews_backend /etc/nginx/sites-available/worldnews_backend
+ADD configs/site_headlinie_api /etc/nginx/sites-available/headlinie_api
 ADD configs/nginx.conf /etc/nginx/nginx.conf
-RUN ln -s /etc/nginx/sites-available/worldnews_backend /etc/nginx/sites-enabled/worldnews_backend
+RUN ln -s /etc/nginx/sites-available/headlinie_api /etc/nginx/sites-enabled/headlinie_api
 
 # Running stuff
 
 EXPOSE 8000
 #CMD /usr/sbin/php5-fpm -y /etc/php5/fpm/php-fpm.conf -c /etc/php5/fpm/php.ini && /usr/sbin/nginx -c /etc/nginx/nginx.conf
-CMD sh /worldnews/scripts/run-app.sh -c -n -f
+CMD sh /headlinie/scripts/run-app.sh -c -n -f
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,44 +1,62 @@
 #! /bin/bash
 
 #
-# WorldNews Backend
+# Headlinie API
 #
 
 DEV=$1
+CACHE=$2
+
+if [ "$DEV" == "--dev" ]; then
+  # Nada
+  echo
+else
+  CACHE=$1
+  echo "Building from scratch without any cache!"
+fi
+
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 echo "";
 echo "### Building image";
 echo "";
 
-docker build -t worldnews/backend .
+if [ "$CACHE" == "--no-cache" ]; then
+  docker build --no-cache -t headlinie/api .
+else
+  docker build -t headlinie/api .
+fi
 
 echo "";
-echo "## Running container from image";
+echo -e "\033[33;32m## Running container from image";
 echo "";
 
 if [ "$DEV" == "--dev" ]; then
   # Interactive
-  echo "##### Interactive session with bash and synced folders"
-  echo "## Source located in /worldnews/"
-  echo "##"
+  echo -e "\033[33;35m##### Interactive session with bash and synced folders"
+  echo -e "##\033[33;34m Source located in /headlinie/"
+  echo -e "\033[33;35m##"
   echo "## To start php5-fpm"
-  echo "##   /usr/sbin/php5-fpm -y /etc/php5/fpm/php-fpm.conf -c /etc/php5/fpm/php.ini"
-  echo "##"
+  echo -e "## \033[33;34m  /usr/sbin/php5-fpm -y /etc/php5/fpm/php-fpm.conf -c /etc/php5/fpm/php.ini"
+  echo -e "\033[33;35m##"
+  echo "## To start php5-fpm"
+  echo -e "##  \033[33;34m /usr/sbin/php5-fpm -y /etc/php5/fpm/php-fpm.conf"
   echo "## To start nginx:"
-  echo "##   /usr/sbin/nginx -c /etc/nginx/nginx.conf"
-  echo "##"
+  echo -e "##  \033[33;34m /usr/sbin/nginx -c /etc/nginx/nginx.conf"
+  echo -e "\033[33;35m##"
   echo "#####"
   echo "##"
-  echo "## OR start using script provided in /worldnews/scripts"
-  echo "##   sh /worldnews/scripts/run-app.sh"
+  echo "## OR start using script provided in /headlinie/scripts"
+  echo -e "##  \033[33;34m sh /headlinie/scripts/run-app.sh"
+  echo -e "\033[33;35m##"
+  echo "## The following arguments are available for starting different services"
   echo "##"
-  echo "## OR start same script with installing composer first"
-  echo "##   sh /worldnews/scripts/run-app.sh --composer "
-  echo "##"
+  echo -e "\033[33;35m##  \033[33;34m -c Install composer dependencies"
+  echo -e "\033[33;35m##  \033[33;34m -f Start PHP-FPM"
+  echo -e "\033[33;35m##  \033[33;34m -n Start Nginx"
+  echo -e "\033[33;35m##"
   echo "#####"
-  #docker run -p 8000:8000 -i -t WorldNews/Backend /bin/bash
-  docker run -p 8000:8000 -v /home/victor/Projects/worldnewsBackend:/worldnews/:rw -i -t worldnews/backend /bin/bash
+  docker run -p 8000:8000 -v $WORKING_DIR:/headlinie/:rw -i -t headlinie/api /bin/bash
 else
   # Non-interactive
   echo "#####"
@@ -46,5 +64,5 @@ else
   echo "## Server started on port 8000"
   echo "##"
   echo "#####"
-  docker run -p 8000:8000 -i -t worldnews/backend
+  docker run -p 8000:8000 -i -t headlinie/api&
 fi
